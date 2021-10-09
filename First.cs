32e40427e3732790d0479cf85028871b7d386987
GitRaut <92181249+GitRaut@users.noreply.github.com>
2021-10-09 10:22:26 +0300

    Add files via upload

---
+++ First.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WASP_ACADEMY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int first = int.Parse(Console.ReadLine());
+			int second = int.Parse(Console.ReadLine());
+			int third = int.Parse(Console.ReadLine());
+
+			if ((first == second) & (first == third))
+			{
+				Console.WriteLine(3);
+			}
+			if ((first == second) & (first != third))
+			{
+				Console.WriteLine(2);
+			}
+			if ((first == third) & (first != second))
+			{
+				Console.WriteLine(2);
+			}
+			if((first != second) & (first != third) & (second != third))
+			{
+				Console.WriteLine(0);
+			}
+		}
+    }
+}
