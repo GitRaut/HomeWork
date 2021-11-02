171c9b389486e5f708ca9bc0869aeaaa80d1c983
GitRaut <92181249+GitRaut@users.noreply.github.com>
2021-11-03 01:52:32 +0300

    HomeWork

--- Задачки/Homework/Level B/TaskB2.cs
+++ Задачки/Homework/Level B/TaskB2.cs
@@ -20,9 +20,36 @@ namespace Homework
     {
         public static List<int> OrderWeight(List<int> list)
         {
-            // Здесь необходимо написать код.
-
-            return null;
+            List<int> prom = new List<int>();
+            List<int> result = new List<int>();
+            int sum = 0, flag = 0;
+            for(int i = 0; i < list.Count; i++)
+            {
+                while(list[i] > 0)
+                {
+                    sum += list[i] % 10;
+                    list[i] = list[i] / 10;
+                }
+                prom.Add(sum);
+                sum = 0;
+            }
+            for (int i = 0; i < prom.Count; i++)
+            {
+                for (int j = 0; j < prom.Count - 1; j++)
+                {
+                    if (prom[j] < prom[j + 1])
+                    {
+                        flag = prom[j];
+                        prom[j] = prom[j + 1];
+                        prom[j + 1] = flag;
+                    }
+                }
+            }
+            for(int i = prom.Count - 1; i > 0; i--)
+            {
+                result.Add(prom[i]);
+            }
+            return result;
         }
     }
 }
