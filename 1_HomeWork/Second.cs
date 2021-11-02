568faa08499fe9d6b6f413f03b3a3b868eeead1c
GitRaut <92181249+GitRaut@users.noreply.github.com>
2021-11-02 14:08:10 +0300

    HomeWork

---
+++ 1_HomeWork/Second.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WASP_ACADEMY
+{
+	class Second
+	{
+		static void Main(string[] args)
+		{
+			int x = int.Parse(Console.ReadLine());
+			int y = int.Parse(Console.ReadLine());
+			int x1 = int.Parse(Console.ReadLine());
+			int y1 = int.Parse(Console.ReadLine());
+
+			if ((x != x1) & (y != y1))
+			{
+				Console.WriteLine("NO");
+			}
+			if ((x == x1) & (y == y1))
+			{
+				Console.WriteLine("NO");
+			}
+			if ((x == x1) && (y != y1))
+			{
+				Console.WriteLine("YES");
+			}
+			if ((y == y1) && (x != x1))
+			{
+				Console.WriteLine("YES");
+			}
+		}
+	}
+}
