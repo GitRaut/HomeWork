c70ca8f5e68de15d61f8413cd4ecfa4a5743bb9a
GitRaut <92181249+GitRaut@users.noreply.github.com>
2021-11-09 19:07:49 +0300

    Metropoliten1

---
+++ Metropoliten1/WASP_ACADEMY/Station.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WASP_ACADEMY
+{
+    public class Station
+    {
+        string name;
+        Line line;
+        bool isWheelChairAccessible;
+        bool hasParkAndRide;
+        bool hasNearbyCableCar;
+        List<Station> transfers;
+
+        public Station(string name)
+        {
+            this.name = name;
+
+            this.transfers = new List<Station>();
+        }
+        public Station(string name, List<Station> transfers)
+        {
+            this.name = name;
+            this.transfers = transfers;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public bool IsWheelChairAccessible()
+        {
+            return isWheelChairAccessible;
+        }
+        public bool HasParkAndRide()
+        {
+            return hasParkAndRide;
+        }
+        public bool HasNearbyCableCar()
+        {
+            return hasNearbyCableCar;
+        }
+        public string GetLineName()
+        {
+            return line.GetName();
+        }
+        public List<Station> GetTransferList()
+        {
+            return transfers;
+        }
+    }
+}
