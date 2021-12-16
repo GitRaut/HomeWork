05032ec90a05fa0bac60e340dd753c89a8a8fff2
GitRaut <92181249+GitRaut@users.noreply.github.com>
2021-12-16 19:40:57 +0300

    NewPin

--- WPF/pin/pin/MainWindow.xaml.cs
+++ WPF/pin/pin/MainWindow.xaml.cs
@@ -31,83 +31,11 @@ namespace pin
             InitializeComponent();
         }
 
-        private void one_Click(object sender, RoutedEventArgs e)
+        private void _Click(object sender, RoutedEventArgs e)
         {
             if (count != 1)
             {
-                enter.Text += "1";
-            }
-        }
-
-        private void two_Click(object sender, RoutedEventArgs e)
-        {
-            if (count != 1)
-            {
-                enter.Text += "2";
-            }
-        }
-
-        private void three_Click(object sender, RoutedEventArgs e)
-        {
-            if (count != 1)
-            {
-                enter.Text += "3";
-            }
-        }
-
-        private void four_Click(object sender, RoutedEventArgs e)
-        {
-            if (count != 1)
-            {
-                enter.Text += "4";
-            }
-        }
-
-        private void five_Click(object sender, RoutedEventArgs e)
-        {
-            if (count != 1)
-            {
-                enter.Text += "5";
-            }
-        }
-
-        private void six_Click(object sender, RoutedEventArgs e)
-        {
-            if (count != 1)
-            {
-                enter.Text += "6";
-            }
-        }
-
-        private void seven_Click(object sender, RoutedEventArgs e)
-        {
-            if (count != 1)
-            {
-                enter.Text += "7";
-            }
-        }
-
-        private void eight_Click(object sender, RoutedEventArgs e)
-        {
-            if (count != 1)
-            {
-                enter.Text += "8";
-            }
-        }
-
-        private void nine_Click(object sender, RoutedEventArgs e)
-        {
-            if (count != 1)
-            {
-                enter.Text += "9";
-            }
-        }
-
-        private void nol_Click(object sender, RoutedEventArgs e)
-        {
-            if(count != 1)
-            {
-                enter.Text += "0";
+                enter.Text += (sender as Button).Content;
             }
         }
 
